6bf6362ef1d8feac1bc3841f1f6b6e454fc74fac
hoboarts <87020885+hoboarts@users.noreply.github.com>
2021-08-20 19:50:53 +0300

    pay issue solution

--- frontend/src/components/PayButton.js
+++ frontend/src/components/PayButton.js
@@ -35,35 +35,48 @@
 
 
 import Axios from 'axios';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { translate } from '../localisation';
 import LoadingBox from './LoadingBox';
 
 export default function PayButton(props) {
     const { rate, lang } = props;
-    const payHandler = async () => {
-        const { data } = await Axios.get('/api/configs/paymer');
-        const button = window.$ipsp.get('button');
-        button.setMerchantId(data);
-        button.setAmount(props.amount, 'UAH', true);
-        button.setHost('api.tascombank.com.ua');
-        button.setResponseUrl('http://localhost:3000/api/orders/verif');
-        button.addField({
-            'label': 'orderId',
-            'name': `${props.amount}00`,
-            'value': props.orderid,
-            'readonly': true
-        });
-        window.location.href = button.getUrl();
+    const [dat, setDat] = useState("");
+
+    const payHandler = () => {
+        if (dat) {
+            const button = window.$ipsp.get('button');
+            button.setMerchantId(dat.merchId);
+            button.setAmount(props.amount, 'UAH', true);
+            button.setHost('api.tascombank.com.ua');
+            button.setResponseUrl(`${dat.hostName}/api/orders/verif`);
+            button.addField({
+                'label': 'orderId',
+                'name': `${props.amount}00`,
+                'value': props.orderid,
+                'readonly': true
+            });
+            window.location.href = button.getUrl();
+        }
     };
-    useEffect(() => {
+    const grabData = async () => {
+        const { data } = await Axios.get('/api/configs/paymer');
+        setDat(data);
+    }
+    const createScript = () => {
         const script = document.createElement('script');
         script.src = "https://api.tascombank.com.ua/static_common/v1/checkout/ipsp.js";
         script.type = "text/javascript";
         script.async = true;
         document.body.appendChild(script);
-    });
-    return ((!rate || isNaN(rate) || rate === null) ? <LoadingBox></LoadingBox> :
+    }
+    useEffect(() => {
+        if (!dat) {
+            createScript();
+            grabData();
+        }
+    }, [dat]);
+    return ((!rate || isNaN(rate) || rate === null || !dat || !window.$ipsp) ? <LoadingBox></LoadingBox> :
         <div>
             <button className="primary block" onClick={payHandler} amount={0} orderid={'0'}>{translate(lang, 'pay_Name')}</button>
         </div>
