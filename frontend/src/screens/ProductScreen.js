1abcddfe6a7d6aa26e1aa397a8bb6c04bacc9cde
Simon Orlov <hoboart@zoho.com>
2021-09-07 17:49:57 +0300

    little fix on prod scr

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -174,7 +174,7 @@ export default function ProductScreen(props) {
                                                     <div className="row">
                                                         <div>{translate(lang, 'qty_Name')}</div>
                                                         <div>
-                                                            <select value={qty} onChange={e => setQty(e.target.value)}>
+                                                            <select title="qty_select" value={qty} onChange={e => setQty(e.target.value)}>
                                                                 {[...Array(product.countInStock).keys()].map(x => (
                                                                     <option key={x + 1} value={x + 1}>{x + 1}</option>
                                                                 ))}
